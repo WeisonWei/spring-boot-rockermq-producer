1410460c099c2a3c02ff218d52bbd5c20c2b7779
weixiaoxing <weixiaoxing@meishubao.com>
2020-09-25 21:59:35 +0800

    add submodule

---
+++ src/test/java/com/weison/sbrp/mq/ConsumerTest.java
@@ -0,0 +1,45 @@
+package com.weison.sbrp.mq;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+/**
+ * @author Weison
+ * @date 2020/9/25
+ */
+public class ConsumerTest {
+
+    @Test
+    public void consumer() throws InterruptedException, MQClientException {
+        // Instantiate with specified consumer group name.
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name");
+
+        // Specify name server addresses.
+        consumer.setNamesrvAddr("localhost:9876");
+
+        // Subscribe one more more topics to consume.
+        consumer.subscribe("TopicTest", "*");
+        // Register callback to execute on arrival of messages fetched from brokers.
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+                                                            ConsumeConcurrentlyContext context) {
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        //Launch the consumer instance.
+        consumer.start();
+
+        System.out.printf("Consumer Started.%n");
+    }
+}
